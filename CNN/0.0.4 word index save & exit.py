f627209eb998cdd07c4e8b66c94e89728f1c1007
kimardenmiller <kimardenmiller@gmail.com>
2017-04-28 11:57:50 -0700

    - Generating predictions on selected data set

---
+++ CNN/0.0.4 word index save & exit.py
@@ -0,0 +1,153 @@
+# from __future__ import print_function
+import os
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils.np_utils import to_categorical
+from keras.layers import Dense, Input, Flatten
+from keras.layers import Conv1D, MaxPooling1D, Embedding
+from keras.models import Model
+import sys
+import pandas as pd
+import pickle
+from keras.models import load_model
+
+np.random.seed(1337)
+
+BASE_DIR = '/Users/kimardenmiller/local_data/NLP_Embeddings/'
+GLOVE_DIR = BASE_DIR + 'glove.6B/'
+TEXT_DATA_DIR = BASE_DIR + '20_newsgroup/'
+MAX_SEQUENCE_LENGTH = 1000
+MAX_NB_WORDS = 20000
+EMBEDDING_DIM = 100
+VALIDATION_SPLIT = 0.2
+
+# first, build index mapping words in the embeddings set to their embedding vector
+print('Indexing word vectors.')
+
+embeddings_index = {}
+
+f = pd.read_csv(os.path.join(GLOVE_DIR, 'glove.6B.100d_2.txt'), sep=" ", header=None).values
+for line in f:
+    word = line[0]
+    coefficients = np.asarray(line[1:], dtype='float32')
+    embeddings_index[word] = coefficients
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+# second, prepare text samples and their labels
+print('Processing text data set')
+
+texts = []  # list of text samples
+labels_index = {}  # dictionary mapping label news_group to numeric id
+labels = []  # list of label ids
+for news_group in sorted(os.listdir(TEXT_DATA_DIR)):
+    path = os.path.join(TEXT_DATA_DIR, news_group)
+    if os.path.isdir(path):
+        label_id = len(labels_index)
+        labels_index[news_group] = label_id
+        for post in sorted(os.listdir(path)):
+            if post.isdigit():
+                post_path = os.path.join(path, post)
+                if sys.version_info < (3,):
+                    f = open(post_path)
+                else:
+                    f = open(post_path, encoding='latin-1')
+                texts.append(f.read())
+                f.close()
+                labels.append(label_id)
+
+print('Found %s texts.' % len(texts))
+print(texts[0])
+
+# finally, vectorize the text samples into a 2D integer tensor
+tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)
+tokenizer.fit_on_texts(texts)
+pickle.dump(tokenizer, open('../saved_models/tokenizer.004.p', 'wb'))
+sequences = tokenizer.texts_to_sequences(texts)
+
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+print('Word index', word_index.values())
+
+data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+labels = to_categorical(np.asarray(labels))
+print('Shape of data tensor:', data.shape)
+print('Sample data tensor:', data[0, 0:])
+print('Shape of label tensor:', labels.shape)
+print('Sample label tensor:', labels[690:750, 0:])
+# sys.exit()
+
+# split the data into a training set and a validation set
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+nb_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
+
+x_train = data[:-nb_validation_samples]
+y_train = labels[:-nb_validation_samples]
+x_val = data[-nb_validation_samples:]
+y_val = labels[-nb_validation_samples:]
+
+print('Preparing embedding matrix.')
+
+# prepare embedding matrix
+nb_words = min(MAX_NB_WORDS, len(word_index))
+embedding_matrix = np.zeros((nb_words + 1, EMBEDDING_DIM))
+for word, i in word_index.items():
+    if i > MAX_NB_WORDS:
+        continue
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector
+
+# load pre-trained word embeddings into an Embedding layer
+# trainable = False so as to keep the embeddings fixed
+embedding_layer = Embedding(nb_words + 1,
+                            EMBEDDING_DIM,
+                            weights=[embedding_matrix],
+                            input_length=MAX_SEQUENCE_LENGTH,
+                            trainable=False)
+
+print('Training model.')
+
+# train a 1D convnet with global maxpooling
+sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+embedded_sequences = embedding_layer(sequence_input)
+x = Conv1D(128, 5, activation='relu')(embedded_sequences)
+x = MaxPooling1D(5)(x)
+x = Conv1D(128, 5, activation='relu')(x)
+x = MaxPooling1D(5)(x)
+x = Conv1D(128, 5, activation='relu')(x)
+x = MaxPooling1D(35)(x)
+x = Flatten()(x)
+x = Dense(128, activation='relu')(x)
+preds = Dense(len(labels_index), activation='softmax')(x)
+
+model = Model(sequence_input, preds)
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['acc'])
+
+model.fit(x_train, y_train, validation_data=(x_val, y_val),
+          nb_epoch=2, batch_size=128)
+
+#  Dec 30, 2016  loss: 0.3069 - acc: 0.8908 - val_loss: 0.1421 - val_acc: 0.9549
+
+# First evaluation of the model
+scores = model.evaluate(x_val, y_val, verbose=0)
+print("Accuracy of Run Model: %.2f%%" % (scores[1]*100))
+
+model.save('../saved_models/0.0.4_model.h5')
+np.save('../saved_models/0.0.4_word_index.npy', word_index)
+
+del model  # deletes the existing model
+
+# returns a compiled model identical to the previous one
+model = load_model('../saved_models/0.0.4_model.h5')
+
+# Final evaluation of the model
+scores = model.evaluate(x_val, y_val, verbose=0)
+print("Accuracy of Disk-Loaded Model: %.2f%%" % (scores[1]*100))
---
+++ CNN_LSTM/0.0.1 from CNN 0.0.3 model save .py
@@ -0,0 +1,185 @@
+# from __future__ import print_function
+import os
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils.np_utils import to_categorical
+from keras.layers import Dense, Input, Flatten
+from keras.layers import Conv1D, MaxPooling1D, Embedding
+from keras.layers import LSTM
+from keras.models import Model
+
+from keras.models import Sequential
+
+import sys
+import pandas as pd
+from keras.models import load_model
+
+np.random.seed(1337)
+
+BASE_DIR = '/Users/kimardenmiller/local_data/NLP_Embeddings/'
+GLOVE_DIR = BASE_DIR + 'glove.6B/'
+TEXT_DATA_DIR = BASE_DIR + '20_newsgroup/'
+MAX_SEQUENCE_LENGTH = 1000
+MAX_NB_WORDS = 20000
+EMBEDDING_DIM = 100
+VALIDATION_SPLIT = 0.2
+
+# first, build index mapping words in the embeddings set to their embedding vector
+print('Indexing word vectors.')
+
+embeddings_index = {}
+
+f = pd.read_csv(os.path.join(GLOVE_DIR, 'glove.6B.100d_2.txt'), sep=" ", header=None).values
+for line in f:
+    word = line[0]
+    coefficients = np.asarray(line[1:], dtype='float32')
+    embeddings_index[word] = coefficients
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+# second, prepare text samples and their labels
+print('Processing text data set')
+
+texts = []  # list of text samples
+labels_index = {}  # dictionary mapping label news_group to numeric id
+labels = []  # list of label ids
+for news_group in sorted(os.listdir(TEXT_DATA_DIR)):
+    path = os.path.join(TEXT_DATA_DIR, news_group)
+    if os.path.isdir(path):
+        label_id = len(labels_index)
+        labels_index[news_group] = label_id
+        for post in sorted(os.listdir(path)):
+            if post.isdigit():
+                post_path = os.path.join(path, post)
+                if sys.version_info < (3,):
+                    f = open(post_path)
+                else:
+                    f = open(post_path, encoding='latin-1')
+                texts.append(f.read())
+                f.close()
+                labels.append(label_id)
+
+print('Found %s texts.' % len(texts))
+print(texts[0])
+
+# finally, vectorize the text samples into a 2D integer tensor
+tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)
+tokenizer.fit_on_texts(texts)
+# pickle.dump(tokenizer, open('../saved_models/tokenizer.007.p', 'wb'))
+sequences = tokenizer.texts_to_sequences(texts)
+
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+print('Word index', word_index.values())
+
+data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+labels = to_categorical(np.asarray(labels))
+print('Shape of data tensor:', data.shape)
+print('Sample data tensor:', data[0, 0:])
+print('Shape of label tensor:', labels.shape)
+print('Sample label tensor:', labels[690:750, 0:])
+# sys.exit()
+
+# split the data into a training set and a validation set
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+nb_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
+
+x_train = data[:-nb_validation_samples]
+y_train = labels[:-nb_validation_samples]
+x_val = data[-nb_validation_samples:]
+y_val = labels[-nb_validation_samples:]
+
+print('Preparing embedding matrix.')
+
+# prepare embedding matrix
+nb_words = min(MAX_NB_WORDS, len(word_index))
+embedding_matrix = np.zeros((nb_words + 1, EMBEDDING_DIM))
+for word, i in word_index.items():
+    if i > MAX_NB_WORDS:
+        continue
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector
+
+# load pre-trained word embeddings into an Embedding layer
+# trainable = False so as to keep the embeddings fixed
+embedding_layer = Embedding(nb_words + 1,
+                            EMBEDDING_DIM,
+                            weights=[embedding_matrix],
+                            input_length=MAX_SEQUENCE_LENGTH,
+                            trainable=False)
+
+print('Training model.')
+
+# train a 1D convnet with global maxpooling
+# sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+# embedded_sequences = embedding_layer(sequence_input)
+# model = Sequential()
+# model.add(embedding_layer(sequence_input))
+# # model.add(embedded_sequences)
+# model.add(Conv1D(128, 5, activation='relu'))
+# model.add(MaxPooling1D(5))
+#
+# model.add(Conv1D(128, 5, activation='relu'))
+# model.add(MaxPooling1D(5))
+#
+# model.add(Conv1D(128, 5, activation='relu'))
+# model.add(MaxPooling1D(35))
+#
+# model.add(Flatten())
+# model.add(Dense(128, activation='relu'))
+# model.add(Dense(len(labels_index), activation='softmax'))
+
+sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+embedded_sequences = embedding_layer(sequence_input)
+x = Conv1D(128, 5, activation='relu')(embedded_sequences)
+x = MaxPooling1D(5)(x)
+x = Conv1D(128, 5, activation='relu')(x)
+x = MaxPooling1D(5)(x)
+x = Conv1D(128, 5, activation='relu')(x)
+x = MaxPooling1D(35)(x)
+x = Flatten()(x)
+x = Dense(128, activation='relu')(x)
+preds = Dense(len(labels_index), activation='softmax')(x)
+model = Model(sequence_input, preds)
+
+
+# create LSTM model
+# embedding_vector_length = 32
+# model = Sequential()
+# model.add(Embedding(top_words, embedding_vector_length, input_length=max_review_length, dropout=0.2))
+# model.add(Convolution1D(nb_filter=32, filter_length=3, border_mode='same', activation='relu'))
+# model.add(MaxPooling1D(pool_length=2))
+# model.add(LSTM(100, dropout_W=0.2, dropout_U=0.2))
+# model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['acc'])
+
+model.fit(x_train, y_train, validation_data=(x_val, y_val),
+          nb_epoch=2, batch_size=128)
+
+# First evaluation of the model
+scores = model.evaluate(x_val, y_val, verbose=0)
+print("Accuracy of Model on val: %.2f%%" % (scores[1]*100))
+
+model.save('../saved_models/0.0.3_model.h5')
+
+del model  # deletes the existing model
+
+# returns a compiled model identical to the previous one
+model = load_model('../saved_models/0.0.3_model.h5')
+
+# Final evaluation of the model
+scores = model.evaluate(x_val, y_val, verbose=0)
+print("Accuracy of Disk-Loaded Model: %.2f%%" % (scores[1]*100))
+
+#  Mar 8, 2017 Accuracy of Disk-Loaded Model: 93.32%
+#  Mar 8, 2017 Accuracy of Disk-Loaded Model: 88.22%
+#  Mar 23, 2017 Accuracy of Disk-Loaded Model: 89.00%
